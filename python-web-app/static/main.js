0febc379effc9ef85f2fba1114c3b7ae0d7fb429
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-12-04 08:29:42 +0900

    canvas手書き機能実装

---
+++ python-web-app/static/main.js
@@ -0,0 +1,7 @@
+const canvasElement = document.getElementById('draw-area')
+const canvas = new HandwritingCanvas(canvasElement)
+
+const clearButtonElement = document.getElementById('clear-button')
+clearButtonElement.addEventListener('click', () => {
+  canvas.clear()
+})
