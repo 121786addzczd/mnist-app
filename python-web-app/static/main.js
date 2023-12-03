6a11f8cc5ec48f74ba8e517e819d5e92da8462f0
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-12-04 08:48:14 +0900

    推論結果を画面に表示

--- python-web-app/static/main.js
+++ python-web-app/static/main.js
@@ -10,5 +10,29 @@ const predictButtonElement = document.getElementById('predict-button')
 predictButtonElement.addEventListener('click', async () => {
   const response = await fetch('/api/predict')
   const responseData = await response.json()
-  console.log(responseData)
+
+  // 推論結果をtbodyに表示
+
+  const tableBodyElement = document.getElementById('result-table-body')
+
+  while (tableBodyElement.firstChild) {
+    tableBodyElement.removeChild(tableBodyElement.firstChild)
+  }
+
+  const probabilities = responseData.probabilities
+    for (let i = 0; i < probabilities.length; i++) {
+      const tr = document.createElement('tr')
+
+      // 数字
+      const tdNumber = document.createElement('td')
+      tdNumber.textContent = i
+      tr.appendChild(tdNumber)
+
+      // 確率
+      const tdProbability = document.createElement('td')
+      tdProbability.textContent = (probabilities[i] * 100).toFixed(1)
+      tr.appendChild(tdProbability)
+
+      tableBodyElement.appendChild(tr)
+  }
 })
