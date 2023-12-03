4039352a35260021fee131e58f852fd7cfe16101
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-12-04 08:34:49 +0900

    JavaScriptとpythonの疎通

--- python-web-app/main.py
+++ python-web-app/main.py
@@ -10,3 +10,8 @@ app.mount("/static", StaticFiles(directory="static"))
 async def root():
     with open("index.html", "r") as f:
         return f.read()
+
+@app.get("/api/predict")
+async def predict():
+    probabilities = [0.2, 0.3, 0.5, 0, 0, 0, 0, 0, 0]
+    return {'probabilities': probabilities}
