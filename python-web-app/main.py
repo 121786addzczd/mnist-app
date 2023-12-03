fe582f4a40df94ab82432f5e7b35f971a3036f56
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2023-12-03 18:33:35 +0900

    FastAPIを使用したHTMLのHello World

--- python-web-app/main.py
+++ python-web-app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-  return {"message": "Hello World"}
+    with open("index.html", "r") as f:
+        return f.read()
